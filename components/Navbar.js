1a037e2886cf37517e8c4bc196cb08af6b1f4ab4
Kioshi Okamoto <kjor29@gmail.com>
2021-04-26 11:21:13 -0500

    :dart: IMPROVE: Manejo de logout y navbar condicional

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,46 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { useContext } from 'react'
-import AuthContext from '../stores/authContext'
+import Link from 'next/link';
+import Image from 'next/image';
+import { useContext } from 'react';
+import AuthContext from '../stores/authContext';
 
 export default function Navbar() {
-  const {user, login} = useContext(AuthContext);
+	const { user, login, logout, authReady } = useContext(AuthContext);
 
-  return (
-    <div className="container">
-      <nav>
-        <Image src="/rupee.png" width={50} height={48} />
-        <h1>Gaming Vibes</h1>
-        <ul>
-          <li><Link href="/"><a>Home</a></Link></li>
-          <li><Link href="/guides"><a>Guides</a></Link></li>
-          <li onClick={login} className="btn">Login/SignUp</li>
-        </ul>
-      </nav>
-      <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
-      </div>
-    </div>
-  )
+	return (
+		<div className="container">
+			<nav>
+				<Image src="/rupee.png" width={50} height={48} />
+				<h1>Gaming Vibes</h1>
+				{authReady && (
+					<ul>
+						<li>
+							<Link href="/">
+								<a>Home</a>
+							</Link>
+						</li>
+						<li>
+							<Link href="/guides">
+								<a>Guides</a>
+							</Link>
+						</li>
+
+						{!user && (
+							<li onClick={login} className="btn">
+								Login/SignUp
+							</li>
+						)}
+						{user && <li>{user.email}</li>}
+						{user && (
+							<li onClick={logout} className="btn">
+								Logout
+							</li>
+						)}
+					</ul>
+				)}
+			</nav>
+			<div className="banner">
+				<Image src="/banner.png" width={966} height={276} />
+			</div>
+		</div>
+	);
 }
--- stores/authContext.js
+++ stores/authContext.js
@@ -9,22 +9,39 @@ const AuthContext = createContext({
 
 export const AuthContextProvider = ({children})=>{
     const [user, setUser] = useState(null)
-    
+    const [authReady, setAuthReady] = useState(false);
+
     useEffect(() => {
         netlifyIdentity.on('login',(user)=>{
             setUser(user)
             netlifyIdentity.close()
         })
+        netlifyIdentity.on('logout',()=>{
+            setUser(null)
+        })
+        netlifyIdentity.on('init',(user)=>{
+            setUser(user)
+            setAuthReady(true)
+        })
+        
         netlifyIdentity.init()
+        return ()=>{
+            netlifyIdentity.off('login')
+            netlifyIdentity.off('logout')
+        }
     }, [])
 
     const login = ()=>{
         netlifyIdentity.open()
     }
 
+    const logout = ()=>{
+        netlifyIdentity.logout()
+    }
 
 
-    const context ={ user, login}
+
+    const context ={ user, login, logout, authReady}
 
     return (
         <AuthContext.Provider value={context}>
