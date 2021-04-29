7e41cc50e2ac487fab834a769f4d87f9f0cd478b
Kioshi Okamoto <kjor29@gmail.com>
2021-04-29 15:06:32 -0500

    :dart: IMPROVE: agregar estados y mostrarlos en UI

--- pages/guides.js
+++ pages/guides.js
@@ -1,25 +1,61 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import AuthContext from '../stores/authContext';
 import styles from '../styles/Guides.module.css';
 
 export default function Guides() {
 	const { user, authReady } = useContext(AuthContext);
+	const [guides, setGuides] = useState(null);
+	const [error, setError] = useState(null);
 
 	useEffect(() => {
 		if (authReady) {
-			fetch('/.netlify/functions/guides', user && {
-				headers: {
-					Authorization: 'Bearer ' + user.token.access_token,
-				},
-			})
-				.then((res) => res.json())
-				.then((data) => console.log(data));
+			fetch(
+				'/.netlify/functions/guides',
+				user && {
+					headers: {
+						Authorization: 'Bearer ' + user.token.access_token,
+					},
+				}
+			)
+				.then((res) => {
+					if (!res.ok) {
+						throw Error('You must logged in to view this content');
+					}
+					return res.json();
+				})
+				.then((data) => {
+					setGuides(data);
+					setError(null);
+				})
+				.catch((err) => {
+					setError(err.message);
+					setGuides(null);
+				});
 		}
 	}, [user]);
 
 	return (
 		<div className={styles.guides}>
-			<h2>All Guides</h2>
+			{!authReady && <div>Loading...</div>}
+
+			{error && (
+				<div className={styles.error}>
+					<p>{error}</p>
+				</div>
+			)}
+
+			{guides &&
+				guides.map((guide) => (
+					<div key={guide.title} className={styles.card}>
+						<h3>{guide.title}</h3>
+						<h4>Written by {guide.author}</h4>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate illum cum iusto amet
+							fugiat. Sequi necessitatibus, eius atque neque eum illum odit labore maiores ratione
+							pariatur veniam quis adipisci quidem.
+						</p>
+					</div>
+				))}
 		</div>
 	);
 }
